0c8c1074cff9f267ebfceb2c1311dc2e4683cd41
Techiesplash <techiesplash137@gmail.com>
2023-01-11 09:05:43 +0000

    Fixed installer

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ make
 # Install
 sudo cp ./parentald /usr/local/bin/parentald
 sudo cp ./parentalctl /usr/local/bin/parentalctl
-sudocp ./init/systemd/parentald.service /etc/systemd/system/parentald.service
+sudo cp ./init/systemd/parentald.service /etc/systemd/system/parentald.service
 sudo chmod 640 /etc/systemd/system/parentald.service
 sudo systemctl daemon-reload
 sudo systemctl enable --now parentald.service
